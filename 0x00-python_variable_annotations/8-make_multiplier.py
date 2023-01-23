350705da6bdaafbc97e114cf80ab78f0170e4eca
cocofox1902 <colas.renard1902@gmail.com>
2023-01-23 13:21:52 +0100

    add

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,7 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable [[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """make_multiplier function"""
     def reterned_function(number: float) -> float:
         """reterned_function function"""
