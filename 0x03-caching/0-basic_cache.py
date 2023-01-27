be36bf4d308befe8e9d4bf189c8fd433d366f4e4
cocofox1902 <colas.renard1902@gmail.com>
2023-01-27 11:34:44 +0100

    add

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -8,12 +8,12 @@ class BasicCache(BaseCaching):
     """ BasicCache """
 
     def put(self, key, item):
-        """ put """
+        """ put function """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ get """
+        """ get function """
         if key in self.cache_data:
             return self.cache_data[key]
         return None
