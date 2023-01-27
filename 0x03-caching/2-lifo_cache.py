b238b60d3e4dbca1ac387dae53011651d062e316
cocofox1902 <colas.renard1902@gmail.com>
2023-01-27 12:02:25 +0100

    add

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -5,17 +5,23 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class LIFOCache(BaseCaching):
     """ LIFOCache """
+
+    def __init__(self):
+        super().__init__()
+
     def put(self, key, item):
-        """ put """
+        """ put function """
         if key and item:
+            if key in list(self.cache_data.keys()):
+                del self.cache_data[key]
+            if (len(self.cache_data.keys()) == self.MAX_ITEMS):
+                k = list(self.cache_data.keys()).pop()
+                del self.cache_data[k]
+                print("DISCARD: {}".format(k))
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last = list(self.cache_data.keys()).pop()
-                print("DISCARD: {}".format(last))
-                del self.cache_data[last]
 
     def get(self, key):
-        """ get """
+        """ get function """
         if key in self.cache_data:
             return self.cache_data[key]
         return None
