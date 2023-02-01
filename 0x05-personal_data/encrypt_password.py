4ff95dd5a63deff9b4627b1cb54789839b3fe206
cocofox1902 <colas.renard1902@gmail.com>
2023-02-01 13:31:44 +0100

    add all

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" encrypt_password.py """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    return bcrypt.checkpw(password.encode(), hashed_password)
---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" filtered_logger.py """
+import mysql.connector
+import datetime
+
+def get_db():
+    connection = mysql.connector.connect(
+        user='root',
+        password='root',
+        host='localhost',
+        database='my_db'
+    )
+    return connection
+
+def main():
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        filtered_data = "name=***; email=***; phone=***; ssn=***; password=***;"
+        log = "[HOLBERTON] user_data INFO {}: {} ip={}; last_login={}; user_agent={};".format(
+            datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S,%f"),
+            filtered_data,
+            row[5],
+            row[6].strftime("%Y-%m-%dT%H:%M:%S"),
+            row[7]
+        )
+        print(log)
+
+if __name__ == "__main__":
+    main()
