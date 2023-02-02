93d3c2a6d764e287515496e3d68db94049a65b38
cocofox1902 <colas.renard1902@gmail.com>
2023-02-02 12:35:37 +0100

    add all

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -7,5 +7,6 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed_password
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     return bcrypt.checkpw(password.encode(), hashed_password)
