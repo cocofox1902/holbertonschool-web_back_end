8c7e40f6b4b974b0d9091940e2e5072c9db78eaa
cocofox1902 <colas.renard1902@gmail.com>
2023-02-09 09:40:42 +0100

    add all

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -39,11 +39,11 @@ def before_request() -> None:
                                         '/api/v1/unauthorized/',
                                         '/api/v1/forbidden/',
                                         '/api/v1/auth_session/login/']):
-        if not auth.current_user(request):
-            abort(403)
         if auth.authorization_header(request) is None and\
            auth.session_cookie(request) is None:
             abort(401)
+        if not auth.current_user(request):
+            abort(403)
         request.current_user = auth.current_user(request)
 
 
--- 0x07-Session_authentication/api/v1/views/users.py
+++ 0x07-Session_authentication/api/v1/views/users.py
@@ -25,7 +25,7 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id is None:
+    if not user_id:
         abort(404)
     if user_id == "me":
         if request.current_user is None:
