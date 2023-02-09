9ce939111d9f06d88c2090b60b2a4b5e18c6354f
cocofox1902 <colas.renard1902@gmail.com>
2023-02-09 11:10:45 +0100

    add 10

--- 0x07-Session_authentication/models/user_session.py
+++ 0x07-Session_authentication/models/user_session.py
@@ -12,6 +12,5 @@ class UserSession(Base):
         """ init method
         """
         super().__init__(*args, **kwargs)
-        if not kwargs:
-            self.user_id = ""
-            self.session_id = ""
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
