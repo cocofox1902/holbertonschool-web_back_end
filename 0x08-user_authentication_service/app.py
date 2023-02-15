2c109c7db89b61aa5070e7dbe4439c338b5e0075
cocofox1902 <colas.renard1902@gmail.com>
2023-02-15 15:19:00 +0100

    add all

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -47,15 +47,55 @@ def login() -> str:
 def logout() -> str:
     """ Logout
     """
-    session_id = request.cookies.get("session_id")
-    if not session_id:
-        abort(403)
-    logged_in_user = AUTH.get_user_from_session_id(session_id)
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            response = redirect("/")
+            response.delete_cookie("session_id")
+            return response
+    return abort(403)
 
-    if not logged_in_user:
+
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """ User profile
+    """
+    session_id = request.cookies.get("session_id", None)
+    if session_id is None:
         abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect('/')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    message = {"email": user.email}
+    return jsonify(message), 200
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ Get reset password token
+    """
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ Update password
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
