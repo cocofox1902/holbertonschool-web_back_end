67a20db747b429fd86f3d7a6264f7f13604cf33f
cocofox1902 <colas.renard1902@gmail.com>
2023-02-15 14:07:39 +0100

    add all

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Flask app
+"""
+from flask import Flask, jsonify, request, abort
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """ Welcome message
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ Register a new user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ Login
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    abort(401)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+""" User module
+"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
+
+
+def _hash_password(password: str) -> str:
+    """ Hash a password
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> None:
+        """ Register a new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validate login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def _generate_uuid(self) -> str:
+        """ Generate a new UUID
+        """
+        return str(uuid.uuid4())
+
+    def create_session(self, email: str) -> str:
+        """ Create a new session
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
