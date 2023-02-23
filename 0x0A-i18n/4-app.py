b4b36747926db4dae62d0dd13583c34bcbcaa774
cocofox1902 <colas.renard1902@gmail.com>
2023-02-23 13:37:31 +0100

    add change

--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -33,7 +33,7 @@ def get_locale():
 def render():
     """ render Function
     """
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
