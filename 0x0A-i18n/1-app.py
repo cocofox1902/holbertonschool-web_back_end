d23655649e7c3be4771394a6b4fd4cf91250d77b
cocofox1902 <colas.renard1902@gmail.com>
2023-02-23 13:19:06 +0100

    add all

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Config class
+    """
+    LANGUAGES = ["en", "fr"]
+
+app.config.from_object(Config)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+@app.route("/", methods=["GET"])
+def render():
+    """ render Function
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
