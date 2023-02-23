e571777f571cf79cb5bb076bf9475f51d5bf9188
cocofox1902 <colas.renard1902@gmail.com>
2023-02-23 17:49:25 +0100

    ad

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
-""" Flask app
+"""
+app module
 """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config(object):
-    """ Config class
-    """
-    LANGUAGES = ["en", "fr"]
-
-
-app.config.from_object(Config)
-Babel.default_locale = 'en'
-Babel.default_timezone = 'UTC'
-
+babel = Babel()
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -27,38 +16,55 @@ users = {
 }
 
 
+@app.before_request
+def before_request():
+    """
+    before_request function
+    """
+    g.user = get_user()
+
+
+class Config(object):
+    """
+    Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel.init_app(app)
+
+
 @babel.localeselector
 def get_locale():
-    """ get locale from request
+    """
+    get_locale function
     """
     locale = request.args.get("locale")
-    if locale:
+    if locale and locale in app.config['LANGUAGES']:
         return locale
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """
+    index route
+    """
+    return render_template("5-index.html")
 
 
 def get_user():
-    """ get user
+    """
+    get_user function
     """
     user_id = request.args.get('login_as')
     if user_id and int(user_id) in users:
         return users[int(user_id)]
     else:
         return None
-
-
-@app.before_request
-def before_request():
-    """ before request
-    """
-    g.user = get_user()
-
-
-@app.route("/", methods=["GET"])
-def render():
-    """ render Function
-    """
-    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
