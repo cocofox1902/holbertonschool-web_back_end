a0e6c827840c90dd87bddc7927e8c6f74c5ab669
cocofox1902 <colas.renard1902@gmail.com>
2023-02-23 17:39:13 +0100

    add

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,36 +1,46 @@
 #!/usr/bin/env python3
-""" Flask app
 """
+Flask app
+"""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config():
-    """ Config class
+    """
+    class config
     """
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-Babel.default_locale = 'en'
-Babel.default_timezone = 'UTC'
+babel = Babel(app)
+babel.init_app(app)
+
+
+@app.route('/')
+def index():
+    """
+    index route
+    """
+
+    title = "Welcome to Holberton"
+    h1 = "Hello world"
+    return render_template("2-index.html", title=title, hello=h1)
 
 
 @babel.localeselector
 def get_locale():
-    ''' get locale from request '''
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
-
-@app.route("/", methods=["GET"])
-def render():
-    """ render Function
     """
-    return render_template("2-index.html")
+    define the best language
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
