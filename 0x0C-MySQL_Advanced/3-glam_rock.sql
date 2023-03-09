eca84833b539ef74c38479025428820e96deeb5a
cocofox1902 <colas.renard1902@gmail.com>
2023-03-09 10:19:21 +0100

    add all

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,4 @@
 -- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, 
-       (YEAR(split)) - (YEAR(formed)) AS lifespand
-FROM metal_bands 
-WHERE style LIKE '%Glam Rock%'
-ORDER BY lifespand DESC;
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%';
--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,9 +1,7 @@
 -- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
 
 CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END;
+FOR EACH ROW UPDATE items
+SET
+quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
