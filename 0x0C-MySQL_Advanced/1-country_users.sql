829efd0df1699934d82cc9911581281e624b778c
cocofox1902 <colas.renard1902@gmail.com>
2023-03-09 10:13:57 +0100

    add all

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Write a SQL script that creates a table users
 
 CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
