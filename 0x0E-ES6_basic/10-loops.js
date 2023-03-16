f328dda6dd81bd6789a51673f00258f133d3821a
cocofox1902 <colas.renard1902@gmail.com>
2023-03-16 07:50:38 +0100

    add 10

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let test = [];
-  for (let idx of array) {
-    test.push(appendString + idx);
+  const array2 = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array2[idx] = appendString + value;
   }
 
-  return test;
+  return array2;
 }
