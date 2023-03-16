031cf261b0048ee101cbb4c8d856b7c76eafc64c
cocofox1902 <colas.renard1902@gmail.com>
2023-03-16 07:56:06 +0100

    add

--- 0x0E-ES6_basic/101-iterateThroughObject.js
+++ 0x0E-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let test = '';
-  for (const item of reportWithIterator) {
-    if (test.length > 0) {
-      test += ' | ';
-    }
-    test += item;
-  }
-  return test;
+  return reportWithIterator.join(' | ');
 }
