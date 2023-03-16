4af03ccf50fcee41c733273f8672d4e1d289b440
cocofox1902 <colas.renard1902@gmail.com>
2023-03-16 07:54:45 +0100

    add

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   const test = [];
   for (const item of Object.values(report.allEmployees)) {
-    test.push(...item);
+    test = [...test, ...item];
   }
   return test;
 }
