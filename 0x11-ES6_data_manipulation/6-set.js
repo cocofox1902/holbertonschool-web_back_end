bb35b0a0353b9f2e68a57a07f2217306a89a562d
cocofox1902 <colas.renard1902@gmail.com>
2023-03-28 10:04:10 +0200

    add format

--- 0x11-ES6_data_manipulation/6-set.js
+++ 0x11-ES6_data_manipulation/6-set.js
@@ -1,3 +1,3 @@
 export default function setFromArray(arrays) {
-  return new Set(arrays)
+  return new Set(arrays);
 }
--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,9 @@ export default function cleanSet(set, startString) {
   for (const el of set) {
     if (el && el.startsWith(startString)) {
       value +=
-        value.length === 0 ? el.replace(startString, '') : el.replace(startString, '-');
+        value.length === 0
+          ? el.replace(startString, '')
+          : el.replace(startString, '-');
     }
   }
   return value;
