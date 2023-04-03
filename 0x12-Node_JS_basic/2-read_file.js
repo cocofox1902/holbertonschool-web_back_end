9c7129691739a964a3acb60bd770cee04f82a5a0
cocofox1902 <colas.renard1902@gmail.com>
2023-04-03 11:19:25 +0200

    add all

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,22 @@
+function countStudents(path) {
+    const fs = require('fs');
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            throw new Error('Cannot load the database');
+        }
+        console.log(`Number of students: ${data.split('\n').length - 1}`);
+        const cs = [];
+        const swe = [];
+        data.split('\n').forEach((line) => {
+            if (line.includes('CS')) {
+                cs.push(line.split(',')[0]);
+            } else if (line.includes('SWE')) {
+                swe.push(line.split(',')[0]);
+            }
+        });
+        console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+        console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+    });
+}
+
+module.exports = countStudents;
